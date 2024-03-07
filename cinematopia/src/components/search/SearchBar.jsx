ed03a67afa76fbef591c608e410ddef1ccea3a06
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-03-07 15:50:44 +0000

    avoiding trouble

--- cinematopia/src/components/search/SearchBar.jsx
+++ cinematopia/src/components/search/SearchBar.jsx
@@ -3,34 +3,6 @@ import { Link } from "react-router-dom";
 import logo from "../../assets/img/cinelogo.png";
 const SearchBar = () => {
   return (
-    // <nav className="flex flex-col lg:flex-row justify-between items-center bg-black text-emerald-50 lg:h-14 py-3 lg:py-0">
-    //   <div className="logo mb-3 lg:mb-0 lg:mr-10">
-    //     <Link to="/">
-    //       <img src={logo} alt="logo" className="w-32" />
-    //     </Link>
-    //   </div>
-    //   <div className="nav-links flex flex-wrap justify-center lg:justify-start">
-    //     <Link to="/" className="mr-4 mb-2 lg:mb-0">
-    //       <span>Home</span>
-    //     </Link>
-    //     <Link to="/genre" className="mr-4 mb-2 lg:mb-0">
-    //       <span>Genre</span>
-    //     </Link>
-    //     <Link to="/movies" className="mr-4 mb-2 lg:mb-0">
-    //       <span>Movies</span>
-    //     </Link>
-    //     <Link to="/tvshows" className="mr-4 mb-2 lg:mb-0">
-    //       <span>TV Shows</span>
-    //     </Link>
-    //   </div>
-    //   <form className="mr-5">
-    //     <input
-    //       type="text"
-    //       className="block w-full lg:w-64 h-8 rounded-md border-0 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-inset focus:ring-gray-400 sm:text-sm sm:leading-6"
-    //       placeholder="Search for your favorite movie or TV show"
-    //     />
-    //   </form>
-    // </nav>
     <nav className="flex flex-col lg:flex-row justify-between items-center  lg:h-14 py-3 lg:py-0">
       <div className="logo mb-3 lg:mb-0 lg:mr-10">
         <Link to="/">
