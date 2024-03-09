da77704abe3b04e6fd57de0dc9251b9efd9cd741
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-03-09 20:06:05 +0000

    styles for movie card

--- cinematopia/src/components/movie-card/MovieCard.jsx
+++ cinematopia/src/components/movie-card/MovieCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import star from "../../assets/img/star.png";
 
-const MovieCard = ({ title, poster, release, type, rating }) => {
+const MovieCard = ({ title, poster, release, rating }) => {
   return (
-    <div className="w-330 transition-transform m-0 flex items-center flex-col p-0 rounded-md shadow-md mr-3 mb-2 bg-stone-700">
+    <div className="w-330 transition-transform m-0 flex items-center flex-col p-0 rounded-md shadow-md mr-3 mb-2 bg-emerald-950">
       <img
         src={poster}
         alt={title}
@@ -11,17 +11,10 @@ const MovieCard = ({ title, poster, release, type, rating }) => {
         style={{ maxWidth: "250px" }}
       />
       <p>{title}</p>
-      <div className="flex pb-2">
+      <div className="flex items-center  justify-center pb-2">
         <p className="mr-2">{release}</p>
-        <p className=" text-base">
-          {rating}
-          <span>
-            <img src={star} className="w-3" />
-          </span>
-        </p>
-        <button className="  mx-auto  px-4  rounded-md cursor-pointer bg-transparent">
-          {type}
-        </button>
+        <img src={star} className="w-7" />
+        <p className=" text-base">{rating}</p>
       </div>
     </div>
   );
