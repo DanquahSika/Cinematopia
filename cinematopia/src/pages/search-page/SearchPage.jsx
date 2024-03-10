200b053ff0b81be511124ae2f58aac78291ec72c
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-03-10 20:44:13 +0000

    creating search page

--- cinematopia/src/pages/search-page/SearchPage.jsx
+++ cinematopia/src/pages/search-page/SearchPage.jsx
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from "react";
 
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <div>
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Search for your favourite movie,tv-show or documentary.."
+      />
+    </div>
+  );
 };
 
 export default SearchPage;
