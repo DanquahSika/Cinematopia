d160b7ff076a18521dd1ad85d83b0b2a4bb1e486
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-03-10 23:49:26 +0000

    added media-type to cards

--- cinematopia/src/components/movie-card/MovieCard.jsx
+++ cinematopia/src/components/movie-card/MovieCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import star from "../../assets/img/star.png";
 import play from "../../assets/img/play.svg";
 
-const MovieCard = ({ title, poster, release, rating }) => {
+const MovieCard = ({ title, poster, release, rating, type }) => {
   return (
     <div
       className="w-330  m-0 flex items-center flex-col p-0 rounded-md shadow-md mr-3 mb-2 relative bg-teal-950 bg-opacity-25"
@@ -26,6 +26,9 @@ const MovieCard = ({ title, poster, release, rating }) => {
       <div className="flex items-center  justify-center pb-2 pt-2">
         <p className="mr-2 font-semibold" style={{ color: "#704912" }}>
           {release}
+        </p>
+        <p className="bg-transparent rounded border text-white w-16 text-center">
+          {type}
         </p>
         <img src={star} className="w-6 mb-1 ml-2" />
         <p className=" font-bold text-lg ml-1">{rating}</p>
