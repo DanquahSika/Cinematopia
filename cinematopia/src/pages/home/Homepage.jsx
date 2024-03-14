9f134b6e4c31d28da9949f99e533ce1ce0bd1b81
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-03-14 03:40:59 +0000

    error fixed

--- cinematopia/src/pages/home/Homepage.jsx
+++ cinematopia/src/pages/home/Homepage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Hero from "../../components/hero/Hero";
-import Login from "../login";
+// import Login from "../login";
 
 const Homepage = () => {
   return (
     <div>
       <Hero />
-      <Login />
+      {/* <Login /> */}
     </div>
   );
 };
