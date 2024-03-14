9caa2b8a97b88514f09745571b32892b4b94804c
Sika Danquah <sikadanquah@Sikas-MacBook-Air.local>
2024-03-14 17:53:41 +0000

    link to home added

--- cinematopia/src/pages/home/Homepage.jsx
+++ cinematopia/src/pages/home/Homepage.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Hero from "../../components/hero/Hero";
-import Login from "../login";
 
 const Homepage = () => {
   return (
     <div>
       <Hero />
-      <Login />
     </div>
   );
 };
