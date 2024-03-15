33fa1c3d07bb13cf2184929da01d3656510cee74
Princess Forson <pribeson121@gmail.com>
2024-03-15 03:22:01 -0700

    new push

--- cinematopia/src/pages/home/Homepage.jsx
+++ cinematopia/src/pages/home/Homepage.jsx
@@ -1,20 +1,10 @@
 import React from "react";
 import Hero from "../../components/hero/Hero";
-import Recommended from "../../components/recommended/Recommended";
-import Featured from "../../components/featured/Featured";
-import Toprated from "../../components/toprated/Toprated";
-import TrendingMovies from "../../components/trending-movies/TrendingMovies";
-import ClassicMoviesAndTrivia from "../../components/classic-and-trivia/ClassicMoviesAndTrivia";
 
 const Homepage = () => {
   return (
     <div>
       <Hero />
-      <Recommended />
-      <Featured />
-      <Toprated />
-      <TrendingMovies />
-      <ClassicMoviesAndTrivia />
     </div>
   );
 };
